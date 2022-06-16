cccf5fdbff344914415f32657b7b0ad213a9d805
Wesley Amaro <contato@wesleyamaro.com>
2022-06-15 19:21:14 -0500

    feat: adding tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
 import Home from './containers/Home';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/index.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
