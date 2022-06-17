085e88856220ffbcf6eb964833cf7363756ff9c2
Wesley Amaro <contato@wesleyamaro.com>
2022-06-17 11:50:10 -0500

    feat: fetching apis

--- src/containers/Details/index.tsx
+++ src/containers/Details/index.tsx
@@ -1,51 +1,131 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProductDetail } from '../../actions/items';
 import Breadcrumb from '../../components/Breadcrumb';
 import Button from '../../components/Button';
 import Header from '../../components/Header';
+import Loader from '../../components/Loader';
 import Main from '../../components/Main';
+import { convertPriceToCurrency } from '../../utils';
+
+interface IAuthor {
+  name: string;
+  lastname: string;
+}
+
+interface IPrices {
+  currency: string;
+  amount: number;
+  decimals: number;
+}
+interface IProduct {
+  id: string;
+  title: string;
+  price: IPrices;
+  picture: string;
+  condition: string;
+  free_shipping: boolean;
+  sold_quantity: number;
+  description: string;
+}
+interface IProductDetail {
+  author: IAuthor;
+  item: IProduct;
+}
 
 function Details() {
+  const params = useParams();
+  const { id: productId } = params;
+
+  const [loading, setLoading] = useState(true);
+  const [productDetail, setProductDetail] = useState<IProductDetail | null>(
+    null
+  );
+
+  const fetchProductDetail = async () => {
+    if (productId) {
+      try {
+        const { data } = await getProductDetail(productId);
+        setProductDetail(data);
+        setLoading(false);
+      } catch {
+        setProductDetail(null);
+        setLoading(false);
+      }
+    } else {
+      setProductDetail(null);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductDetail();
+  }, [productId]);
+
   return (
     <div className="w-full">
       <Header />
       <Breadcrumb />
 
       <Main className="p-8">
-        <div className="w-full flex flex-wrap sm:flex-nowrap">
-          <figure className="w-full inline-flex max-w-2.5xl object-contain rounded-xs mb-4 sm:mb-0">
-            <img src="..." alt="Foto" role="presentation" className="w-full" />
-          </figure>
+        {loading && <Loader />}
 
-          <div className="w-full sm:w-auto flex flex-col">
-            <h2 className="inline-flex text-xs mb-4 text-gray-550">
-              Nuevo - 234 vendidos
-            </h2>
+        {!loading && (
+          <div className="w-full">
+            {productDetail ? (
+              <div className="w-full">
+                <div className="w-full flex flex-wrap sm:flex-nowrap">
+                  <figure className="w-full inline-flex max-w-2.5xl max-h-2.5xl  rounded-xs mb-4 mr-4 sm:mb-0">
+                    <img
+                      src={productDetail.item.picture}
+                      alt="Foto"
+                      role="presentation"
+                      className="w-full object-contain"
+                    />
+                  </figure>
 
-            <h1 className="inline-flex text-2xl text-gray-750 leading-tight">
-              Deco Reverse Sombrero Oxford
-            </h1>
+                  <div className="w-full sm:w-auto flex flex-col">
+                    <h2 className="inline-flex text-xs mb-4 text-gray-550 capitalize">
+                      {`${productDetail.item.condition} - ${productDetail.item.sold_quantity} vendidos`}
+                    </h2>
 
-            <span className="inline-flex my-8 text-4.5xl text-gray-750">
-              $1.980
-            </span>
+                    <h1 className="inline-flex text-2xl text-gray-750 leading-tight">
+                      {productDetail.item.title}
+                    </h1>
 
-            <Button type="button" className="w-full" onChange={() => {}}>
-              Comprar
-            </Button>
+                    <span className="inline-flex my-8 text-4.5xl text-gray-750 whitespace-nowrap">
+                      $
+                      {convertPriceToCurrency(
+                        productDetail.item.price.currency,
+                        productDetail.item.price.amount
+                      )}
+                    </span>
+
+                    <Button
+                      type="button"
+                      className="w-full"
+                      onChange={() => {}}
+                    >
+                      Comprar
+                    </Button>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col mt-8">
+                  <h2 className="inline-flex text-2.5xl text-gray-750 mb-8">
+                    Descripción del producto
+                  </h2>
+
+                  <p className="inline-flex m-0 p-0 text-base text-gray-450">
+                    {productDetail.item.description}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <p>Producto no encontrado...</p>
+            )}
           </div>
-        </div>
-
-        <div className="w-full flex flex-col mt-8">
-          <h2 className="inline-flex text-2.5xl text-gray-750 mb-8">
-            Descripción del producto
-          </h2>
-
-          <p className="inline-flex m-0 p-0 text-base text-gray-450">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
-            blanditiis atque? Perferendis labore, maiores natus architecto atque
-            iure aperiam necessitatibus sed. Dolores pariatur ab qui culpa
-            aspernatur officia doloremque. Deserunt!
-          </p>
-        </div>
+        )}
       </Main>
     </div>
   );
--- src/containers/SearchResults/index.tsx
+++ src/containers/SearchResults/index.tsx
@@ -1,10 +1,41 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Header from '../../components/Header';
 import Breadcrumb from '../../components/Breadcrumb';
 import Main from '../../components/Main';
 import Card from '../../components/Card';
+import useQuery from '../../hooks/useQuery';
+import { getResults } from '../../actions/items';
+import Loader from '../../components/Loader';
 
 function SearchResults() {
+  const query = useQuery();
+  const querySearch = query.get('search') ?? '';
+
+  const [loadingResults, setLoadingResults] = useState(true);
+  const [results, setResults] = useState([]);
+
+  const fetchResults = async () => {
+    if (querySearch) {
+      try {
+        const { data }: any = await getResults(querySearch);
+
+        setResults(data.items);
+        setLoadingResults(false);
+      } catch (err) {
+        setResults([]);
+        setLoadingResults(false);
+      }
+    } else {
+      setResults([]);
+      setLoadingResults(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, [querySearch]);
+
   return (
     <div className="w-full">
       <Header />
@@ -12,18 +43,34 @@ function SearchResults() {
       <Breadcrumb />
 
       <Main className="p-4">
-        <ul className="w-full list-none flex flex-wrap flex-col">
-          <li className="w-full mb-4 last:mb-0 pb-4 last:pb-0 border-b last:border-none border-gray-150">
-            <Link to="/items/1" className="w-full flex flex-nowrap">
-              <Card
-                title="Apple Ipod Touch 5g 16gb negro igual a nuevo completo unico!"
-                location="Mendoza"
-                price="99.99"
-                img="..."
-              />
-            </Link>
-          </li>
-        </ul>
+        {loadingResults && <Loader />}
+
+        {!loadingResults && (
+          <div className="w-full">
+            {results.length > 0 ? (
+              <ul className="w-full list-none flex flex-wrap flex-col">
+                {results.map((item: any) => (
+                  <li
+                    key={item.id}
+                    className="w-full mb-4 last:mb-0 pb-4 last:pb-0 border-b last:border-none border-gray-150 inline-flex"
+                  >
+                    <Card
+                      id={item.id}
+                      title={item.title}
+                      location="Mendoza"
+                      currency={item.price.currency}
+                      price={item.price.amount}
+                      img={item.picture}
+                      freeShipping={item.free_shipping}
+                    />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Ningún resultado encontrado</p>
+            )}
+          </div>
+        )}
       </Main>
     </div>
   );
