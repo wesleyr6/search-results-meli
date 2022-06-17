8c509989e8375003681b0538efcb2594338fdcbe
Wesley Amaro <contato@wesleyamaro.com>
2022-06-17 11:50:04 -0500

    feat: adding uuid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { arrRoutes, IRoutes } from './routes';
 
 function PublicRoutes() {
@@ -8,7 +9,7 @@ function PublicRoutes() {
         {arrRoutes
           .filter((item: IRoutes) => item.active)
           .map(({ path, container: Container }: IRoutes) => (
-            <Route path={path} element={<Container />} />
+            <Route key={uuidv4()} path={path} element={<Container />} />
           ))}
       </Routes>
     </BrowserRouter>
