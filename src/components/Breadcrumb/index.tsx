4cc6bb86cd8d2d616ae85d1a8d2197ac4c6208e8
Wesley Amaro <contato@wesleyamaro.com>
2022-06-18 22:50:07 -0500

    styles: UI

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,18 +1,48 @@
+import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import Wrapper from '../Wrapper';
 
-function Breadcrumb() {
+interface IItem {
+  link?: string;
+  name: string;
+}
+
+interface IProps {
+  items: string[];
+}
+
+function BreadcrumbItem({ name, link }: IItem) {
+  return (
+    <li className="inline-flex text-sm text-gray-450 last:after:hidden after:content-['>'] after:mx-2 last:font-semibold">
+      {link ? (
+        <Link
+          to={link}
+          className="inline-flex text-gray-450 hover:text-gray-450 no-underline hover:underline"
+        >
+          {name}
+        </Link>
+      ) : (
+        <span className="text-gray-450 inline-flex">{name}</span>
+      )}
+    </li>
+  );
+}
+
+BreadcrumbItem.defaultProps = {
+  link: '',
+};
+
+function Breadcrumb({ items }: IProps) {
   return (
     <nav className="w-full">
       <Wrapper>
         <ul className="w-full list-none flex flex-wrap py-4">
-          <li className="inline-flex text-sm text-gray-450 last:after:hidden after:content-['>'] after:mx-2 last:font-semibold">
-            <a
-              href="/"
-              className="text-gray-450 hover:text-gray-450 no-underline hover:underline"
-            >
-              test
-            </a>
-          </li>
+          <BreadcrumbItem key={uuidv4()} name="Home" link="/" />
+
+          {items.length > 0 &&
+            items.map((item: string) => (
+              <BreadcrumbItem key={uuidv4()} name={item} />
+            ))}
         </ul>
       </Wrapper>
     </nav>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,7 +7,7 @@ interface IProps {
   price: number;
   title: string;
   currency: string;
-  img: string;
+  picture: string;
   location?: string;
   freeShipping: boolean;
 }
@@ -16,7 +16,7 @@ function Card({
   id,
   title,
   currency,
-  img,
+  picture,
   price,
   location,
   freeShipping,
@@ -29,7 +29,7 @@ function Card({
       >
         <figure className="min-w-45 min-h-45 max-w-45 max-h-45 overflow-hidden justify-center items-center content-center inline-flex border-none outline-none">
           <img
-            src={img}
+            src={picture}
             alt={title}
             role="presentation"
             className="object-contain w-full h-full inline-flex border-none outline-none bg-white"
@@ -38,20 +38,22 @@ function Card({
       </Link>
 
       <div className="w-full inline-flex flex-col py-4 box-border">
-        <div className="w-full grid grid-cols-4 gap-4 mb-8">
-          <div className="col-span-4 sm:col-span-3 inline-flex items-center content-center">
-            <span className="text-2xl leading-none inline-flex no-underline text-gray-750 whitespace-nowrap">
-              ${convertPriceToCurrency(currency, price)}
-            </span>
+        <div className="w-full grid grid-cols-4 gap-4 sm:gap-8 mb-8">
+          <div className="col-span-4 sm:col-span-3">
+            <div className="w-full inline-flex items-center content-center">
+              <span className="text-2xl leading-none inline-flex no-underline text-gray-750 whitespace-nowrap">
+                ${convertPriceToCurrency(currency, price)}
+              </span>
 
-            {freeShipping && (
-              <img
-                src={IMG_ICON_SHIPMENT}
-                alt="Envío grátis"
-                title="Envío grátis"
-                className="inline-flex ml-2 border-none outline-none"
-              />
-            )}
+              {freeShipping && (
+                <img
+                  src={IMG_ICON_SHIPMENT}
+                  alt="Envío grátis"
+                  title="Envío grátis"
+                  className="inline-flex ml-2 border-none outline-none"
+                />
+              )}
+            </div>
           </div>
 
           <div>
@@ -59,11 +61,11 @@ function Card({
               {location}
             </span>
           </div>
-        </div>
 
-        <h2 className="text-lg leading-6 capitalize text-gray-750 no-underline">
-          {title}
-        </h2>
+          <h2 className="col-span-4 sm:col-span-3 text-lg leading-6 capitalize text-gray-750 no-underline">
+            {title}
+          </h2>
+        </div>
       </div>
     </>
   );
