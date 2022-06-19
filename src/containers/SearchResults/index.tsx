bdc294a19eebd98fab040cdc27fd9f60fe396356
Wesley Amaro <contato@wesleyamaro.com>
2022-06-18 23:21:53 -0500

    feat: adding typos

--- src/containers/SearchResults/index.tsx
+++ src/containers/SearchResults/index.tsx
@@ -7,28 +7,41 @@ import Card from '../../components/Card';
 import useQuery from '../../hooks/useQuery';
 import { getResults } from '../../actions/items';
 import Loader from '../../components/Loader';
+import { IItems, ISearchResults } from '../../interfaces/search-results';
+
+interface IResults {
+  data: ISearchResults;
+}
 
 function SearchResults() {
   const query = useQuery();
   const querySearch = query.get('search') ?? '';
 
-  const [loadingResults, setLoadingResults] = useState(true);
-  const [results, setResults] = useState([]);
+  const [loadingResults, setLoadingResults] = useState<boolean>(true);
+  const [results, setResults] = useState<IItems[]>([]);
+  const [category, setCategory] = useState<string[]>([]);
+
+  const showEmpty = () => {
+    setResults([]);
+    setLoadingResults(false);
+  };
 
   const fetchResults = async () => {
     if (querySearch) {
       try {
-        const { data }: any = await getResults(querySearch);
+        const { data }: IResults = await getResults(querySearch);
 
-        setResults(data.items);
+        setResults([...results, data.items]);
+
+        if (data.category) {
+          setCategory([data.category]);
+        }
         setLoadingResults(false);
       } catch (err) {
-        setResults([]);
-        setLoadingResults(false);
+        showEmpty();
       }
     } else {
-      setResults([]);
-      setLoadingResults(false);
+      showEmpty();
     }
   };
 
@@ -40,7 +53,7 @@ function SearchResults() {
     <div className="w-full">
       <Header />
 
-      <Breadcrumb />
+      <Breadcrumb items={category} />
 
       <Main className="p-4">
         {loadingResults && <Loader />}
@@ -49,19 +62,19 @@ function SearchResults() {
           <div className="w-full">
             {results.length > 0 ? (
               <ul className="w-full list-none flex flex-wrap flex-col">
-                {results.map((item: any) => (
+                {results.map((item: IItems) => (
                   <li
                     key={item.id}
                     className="w-full mb-4 last:mb-0 pb-4 last:pb-0 border-b last:border-none border-gray-150 inline-flex"
                   >
                     <Card
                       id={item.id}
-                      title={item.title}
-                      location="Mendoza"
                       currency={item.price.currency}
-                      price={item.price.amount}
-                      img={item.picture}
                       freeShipping={item.free_shipping}
+                      location={item.location}
+                      picture={item.picture}
+                      price={item.price.amount}
+                      title={item.title}
                     />
                   </li>
                 ))}
